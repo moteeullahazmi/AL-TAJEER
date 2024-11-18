c16d850f8eb90b0e94b06bc7672349cc2fe45018
moteeullahazmi <moteeullahazmi@gmail.com>
2024-11-19 00:57:46 +0530

    443

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "../components/Title";
 import CartTotal from "../components/CartTotal";
 import { assets } from "../assets/frontend_assets/assets";
+import { useNavigate } from "react-router-dom";
 
 function PlaceOrder() {
+  const [method, setMethod] = useState("cod");
+
+  const navigate  = useNavigate();
+
   return (
     <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
       {/* --------LEFT SIDE----------- */}
@@ -73,8 +78,15 @@ function PlaceOrder() {
 
             {/* -----------Payment Method Selection------------- */}
             <div className="flex gap-3 flex-col lg:flex-row">
-              <div className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
-                <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+              <div
+                onClick={() => setMethod("stripe")}
+                className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+              >
+                <p
+                  className={`min-w-3.5 h-3.5 border rounded-full  ${
+                    method === "stripe" ? "bg-green-400" : ""
+                  }`}
+                ></p>
                 <img
                   className="h-5 mx-4"
                   src={assets.stripe_logo}
@@ -82,8 +94,15 @@ function PlaceOrder() {
                 />
               </div>
 
-              <div className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
-                <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+              <div
+                onClick={() => setMethod("razorpay")}
+                className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+              >
+                <p
+                  className={`min-w-3.5 h-3.5 border rounded-full ${
+                    method === "razorpay" ? "bg-green-400" : ""
+                  }`}
+                ></p>
                 <img
                   className="h-5 mx-4"
                   src={assets.razorpay_logo}
@@ -91,10 +110,25 @@ function PlaceOrder() {
                 />
               </div>
 
-              <div className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
-                <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
-                <p className="text-gray-500 text-sm font-medium mx-4">CASH ON DELIVERY</p>
+              <div
+                onClick={() => setMethod("cod")}
+                className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+              >
+                <p
+                  className={`min-w-3.5 h-3.5 border rounded-full ${
+                    method === "cod" ? "bg-green-400" : ""
+                  }`}
+                ></p>
+                <p className="text-gray-500 text-sm font-medium mx-4">
+                  CASH ON DELIVERY
+                </p>
               </div>
+            </div>
+
+            <div className="w-full text-end mt-8">
+              <button onClick={()=> navigate('/orders')} className="bg-black text-white px-16 py-3">
+                PLACE ORDER
+              </button>
             </div>
           </div>
         </div>
