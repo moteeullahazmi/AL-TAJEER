8f84385f94e3fada2c92b57072f2061c61335a69
moteeullahazmi <moteeullahazmi@gmail.com>
2024-11-20 01:59:24 +0530

    backend 524

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -13,9 +13,9 @@ const Contact = () => {
       <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
       <img className="w-full md:max-w-[480px]" src={assets.contact_img} alt="contact_image" />
       <div className="flex flex-col justify-center items-start gap-6">
-        <p className="font-semibold teext-xl text-gray-600">Our Stores</p>
-        <p className=" text-gray-500">Bindra Bazar,<br /> Azamgarh, UP-276205 INDIA</p>
-        <p className="text-gray-500">Tel: +917071277722  <br /> Email: abuazmi143@gmail.com</p>
+        <p className="font-semibold text-xl text-gray-600">Our Store</p>
+        <p className=" text-gray-500">Jamaluddin Apartment <br /> Near Parshuraam Tawre Stadium Dhobi Talao<br/> Bhiwandi, Maharashtra - 421302, India</p>
+        <p className="text-gray-500">Phone: +91 7071277722  <br /> Email: abuazmi143@gmail.com</p>
         <p className="font-semibold text-xl text-gray-600"> Careers at Al Tajeer</p>
         <p className="text-gray-500">Learn more about our teams and job openings.</p>
         <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">Explore Jobs</button>
