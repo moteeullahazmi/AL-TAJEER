91c0c5b62db5e99891ef008d7e664c23143e4221
moteeullahazmi <moteeullahazmi@gmail.com>
2024-11-23 23:05:14 +0530

    update

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -7,7 +7,7 @@ export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
   const currency = "₹";
-  const delivery_fee = 0;
+  const delivery_fee = 40;
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems, setCartItems] = useState([]);
