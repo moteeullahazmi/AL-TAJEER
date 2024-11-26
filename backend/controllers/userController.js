a71900a28731d5d3558d8b9e3b74685a919fa703
moteeullahazmi <moteeullahazmi@gmail.com>
2024-11-26 19:29:50 +0530

    557 complet

---
+++ backend/controllers/userController.js
@@ -0,0 +1,18 @@
+
+
+// Route for user login
+const loginUser = async (req,res) => {
+
+}
+
+// Route for user registation
+const registerUser = async (req,res) => {
+    res.json({msg:"Register API Working"})
+}
+
+// Route for admin login
+const adminLogin = async (req,res) => {
+
+}
+
+export {loginUser, registerUser, adminLogin} 
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,24 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectCloudaniry from "./config/cloudaniry.js";
+import userRouter from "./routes/userRoute.js";
 
 // App Config
 const app =express();
 const port = process.env.PORT || 4000;
+
+connectDB();
+connectCloudaniry();
 
 // middlewares
 app.use(express.json());
 app.use(cors());  //any ip connect
 
 // api endpoint
+app.use("/api/user", userRouter)
+
 app.get("/",(req,res)=>{
     res.send("API Working")
 })
