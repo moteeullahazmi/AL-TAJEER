e0cf96e345a0c289adf821b137c0a99489714e6b
moteeullahazmi <moteeullahazmi@gmail.com>
2024-11-30 21:00:07 +0530

    649

--- backend/server.js
+++ backend/server.js
@@ -2,22 +2,24 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
-import connectCloudaniry from "./config/cloudaniry.js";
+import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
 
 // App Config
 const app =express();
 const port = process.env.PORT || 4000;
 
 connectDB();
-connectCloudaniry();
+connectCloudinary();
 
 // middlewares
 app.use(express.json());
 app.use(cors());  //any ip connect
 
 // api endpoint
-app.use("/api/user", userRouter)
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter)
 
 app.get("/",(req,res)=>{
     res.send("API Working")
