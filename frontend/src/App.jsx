7373528dea625ec72cccaaf3d9d42c2d5fe485d9
moteeullahazmi <moteeullahazmi@gmail.com>
2024-12-06 11:03:57 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,46 +1,34 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import ScrollToTop from './components/ScrollToTop'
-// page import
-import Home from "../src/pages/Home"
-import Collection from "./pages/Collection"
-import About from "./pages/About"
-import Contact from "./pages/Contact"
-import Product from "./pages/Product"
-import Cart from "./pages/Cart"
-import Login from "./pages/Login"
-import PlaceOrder from './pages/PlaceOrder'
-import Orders from "./pages/Orders"
-
-//component import
-import Navbar from "./components/Navbar";
-import Footer from './components/Footer';
-import SearchBar from './components/SearchBar';
-
-// react toastify
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-    <ScrollToTop/>
-    <ToastContainer />
-    <Navbar />
-    <SearchBar/>
-    <Routes>
-          <Route path='/' element= {<Home/>}/>
-          <Route path='/collection' element= {<Collection/>}/>
-          <Route path='/about' element= {<About/>}/>
-          <Route path='/contact' element= {<Contact/>}/>
-          <Route path='/products/:productId' element= {<Product/>}/>
-          <Route path='/cart' element= {<Cart/>}/>
-          <Route path='/login' element= {<Login/>}/>
-          <Route path='/place-order' element= {<PlaceOrder/>}/>
-          <Route path='/orders' element= {<Orders/>}/>
-    </Routes>
-    <Footer/>
-    </div>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
