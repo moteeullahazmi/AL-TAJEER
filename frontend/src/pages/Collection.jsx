e56e3162e82d0e4e8a399bcaf54b5c48caab86bc
moteeullahazmi <moteeullahazmi@gmail.com>
2024-12-06 11:43:31 +0530

    Update

---
+++ frontend/src/pages/Collection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Collection from '../components/Collection'
+
+function Collections() {
+  return (
+    <div>
+      <Collection/>
+    </div>
+  )
+}
+
+export default Collections
